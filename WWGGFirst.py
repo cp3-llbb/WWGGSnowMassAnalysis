34fea10f929d97856ea49812e3e2f22a3946a3c4
Suat Donertas (suat.donertas@uclouvain.be) <sdonerta@ingrid-ui1.cism.ucl.ac.be>
2021-09-23 15:02:24 +0200

    adding more selections

--- WWGGFirst.py
+++ WWGGFirst.py
@@ -49,13 +49,11 @@ class SnowmassExample(CMSPhase2SimRTBHistoModule):
 
         #selection of photons with eta in the detector acceptance
         photons = op.select(t.gamma, lambda ph : op.AND(op.abs(ph.eta)<2.5, ph.pt >25.)) 
-     
+        #selection of photons with loose ID        
         cleanedPhotons = op.select(photons, lambda ph : ph.idpass & (1<<0))
  
         #sort photons by pT 
         sort_ph = op.sort(photons, lambda ph : -ph.pt)
-        #apply pt cut on the leading photon
-        #sorted_ph = op.select(sort_ph, lambda ph : op.AND(ph[0].pt > 35.))  
 
         #sortcleanphotons
         sorted_ph = op.sort(cleanedPhotons, lambda ph : -ph.pt)
@@ -66,17 +64,62 @@ class SnowmassExample(CMSPhase2SimRTBHistoModule):
            (op.in_range(100, op.invariant_mass(sort_ph[0].p4, sort_ph[1].p4), 180)) 
            ))
 
-        mGG = op.invariant_mass(sort_ph[0].p4, sort_ph[1].p4)
-        hGG = op.sum(sort_ph[0].p4, sort_ph[1].p4)
+        mGG = op.invariant_mass(sorted_ph[0].p4, sorted_ph[1].p4)
+        hGG = op.sum(sorted_ph[0].p4, sorted_ph[1].p4)
 
+       #H->WW->2q1l1nu
+       
+        electrons = op.select(t.elec, lambda el : op.AND(
+        el.pt > 10., op.abs(el.eta) < 2.5
+        ))
+        
+        muons = op.select(t.muon, lambda mu : op.AND(
+        mu.pt > 10., op.abs(mu.eta) < 2.5
+        ))
 
-       #selections
+        taus = op.select(t.tau, lambda ta : op.AND(
+        ta.pt > 10., op.abs(ta.eta) < 2.5
+        ))
+        
+        identifiedElectrons = op.select(electrons, lambda el : el.idpass & (1<<0) )  #apply loose ID  
+        cleanedElectrons = op.select(identifiedElectrons, lambda el : op.NOT(op.rng_any(photons, lambda ph : op.deltaR(el.p4, ph.p4) < 0.4))) #apply a deltaR  
+        
+        isolatedMuons = op.select(muons, lambda mu : mu.isopass & (1<<2) ) #apply tight ID & isolation
+        identifiedMuons = op.select(isolatedMuons, lambda mu : mu.idpass & (1<<2) ) 
+        cleanedMuons = op.select(identifiedMuons, lambda mu : op.NOT(op.rng_any(photons, lambda ph : op.deltaR(mu.p4, ph.p4) < 0.4 )))
+    
+        isolatedTaus = op.select(taus, lambda ta : ta.isopass & (1<<2) )
+       # identifiedTaus = op.select(isolatedTaus, lambda ta : ta.idpass & (1<<2) )
+        cleanedTaus = op.select(isolatedTaus, lambda ta : op.NOT(op.rng_any(photons, lambda ph : op.deltaR(ta.p4, ph.p4) < 0.4 )))
+          
+        #select jets with pt>25 GeV end eta in the detector acceptance
+        jets = op.select(t.jetpuppi, lambda jet : op.AND(jet.pt > 30., op.abs(jet.eta) < 2.5))
+        
+        identifiedJets = op.select(jets, lambda j : j.idpass & (1<<2))
+        cleanedJets = op.select(identifiedJets, lambda j : op.AND(
+        op.NOT(op.rng_any(identifiedElectrons, lambda el : op.deltaR(el.p4, j.p4) < 0.4) ),   #identified or cleaned mu/e ? 
+        op.NOT(op.rng_any(identifiedMuons, lambda mu : op.deltaR(mu.p4, j.p4) < 0.4) )
+        ))
+         
+        mJets= op.invariant_mass(cleanedJets[0].p4, cleanedJets[1].p4)
+        hJets = op.sum(cleanedJets[0].p4, cleanedJets[1].p4)
+       
+        #missing transverse energy
+        met = op.select(t.metpuppi)      
 
+        #mHH =
+        #hHH =
+ 
+      #selections
+         
         sel1 = noSel.refine("LPhoton", cut = op.AND((op.rng_len(sort_ph) >= 2), (sort_ph[0].pt > 35.)))
 
         sel2 = noSel.refine("Lphoton", cut = op.AND((op.rng_len(sorted_ph) >= 2), (sorted_ph[0].pt > 35.)))
-
-    #plots
+        
+        sel3 = sel2.refine("TwoPhOneL", cut = op.OR(op.rng_len(cleanedElectrons) >= 1, op.rng_len(cleanedMuons) >= 1, op.rng_len(cleanedTaus) >= 1))
+     
+        sel4 = sel3.refine("TwoPhLNuTwoJ", cut = op.AND((op.rng_len(cleanedJets) >= 2),(met[0].pt > 30)))
+       #plots
        
        #sel1
         plots.append(Plot.make1D("LeadingPhotonPTSel1", sort_ph[0].pt, sel1, EqB(30, 0., 250.), title="Leading Photon pT"))
@@ -89,13 +132,32 @@ class SnowmassExample(CMSPhase2SimRTBHistoModule):
             
         plots.append(Plot.make1D("SubLeadingPhotonPTSel2", sorted_ph[1].pt, sel2, EqB(30, 0., 250.), title="SubLeading Photon pT")) 
        
-    #yields
-       # yields = CutFlowReport("yields", recursive=True, printInLog=True)
-       # plots.append(yields)
-       # yields.add(noSel, title= 'noSel')
-       # yields.add(sel1, title='sel1')
-       # yields.add(sel2, title='sel2')
-       #yields.add(hasTwoPhTwoB, title='2_photons_2_b')
+       #sel3
+        plots.append(Plot.make1D("LeadingPhotonPtSel3", sorted_ph[0].pt, sel3, EqB(30, 0., 250.), title="Leading Photon pT"))
+   
+        plots.append(Plot.make1D("SubLeadingPhotonPtSel3", sorted_ph[1].pt, sel3, EqB(30, 0., 250.), title="SubLeading Photon pT"))
+    
+       #sel4
+        plots.append(Plot.make1D("LeadingPhotonPtSel4", sorted_ph[0].pt, sel4, EqB(30, 0., 250.), title="Leading Photon pT"))
 
+        plots.append(Plot.make1D("SubLeadingPhotonPtSel4", sorted_ph[1].pt, sel4, EqB(30, 0., 250.), title="SubLeading Photon pT"))    
+
+       #diphoton invariant mass plot
+        #plots.append(Plot.make1D("Inv_mass_ggSel1",mGG,sel1,EqB(50, 100.,150.), title = "m_{\gamma\gamma}"))
+       # plots.append(Plot.make1D("Inv_mass_ggSel2",mGG,sel2,EqB(50, 100.,150.), title = "m_{\gamma\gamma}"))
+        plots.append(Plot.make1D("Inv_mass_ggSel3",mGG,sel3,EqB(50, 100.,150.), title = "m_{\gamma\gamma}"))
+        plots.append(Plot.make1D("Inv_mass_ggSel4",mGG,sel4,EqB(50, 100.,150.), title = "m_{\gamma\gamma}"))
+         
+       #HH invariant mass  
+       # plots.append(Plot.make1D("Inv_mass_HH",mHH,sel4,EqB(50, 200.,1000.), title = "m_{HH}"))
+
+       #yields
+        yields = CutFlowReport("yields", recursive=True, printInLog=True)
+        plots.append(yields)
+        yields.add(noSel, title= 'noSel')
+        yields.add(sel1, title='sel1')
+        yields.add(sel2, title='sel2')
+        yields.add(sel3, title='sel3')
+        yields.add(sel4, title='sel4')
 
         return plots
