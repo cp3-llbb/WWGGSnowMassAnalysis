6c0276d379c434270dfafd2fd17c10d9c201e179
Sandhya Jain (sandhya.jain@uclouvain.be) <sjain@ingrid-ui1.cism.ucl.ac.be>
2021-10-14 15:27:04 +0200

    updating the runGetFiles.sh to make sure it runs on all existing samples

--- runGetFiles.sh
+++ runGetFiles.sh
@@ -9,15 +9,15 @@
 ./getFiles.sh GJet_Pt-20toInf_DoubleEMEnriched_MGG-40to80_TuneCUEP8M2T4_14TeV_pythia8_200PU
 ./getTIFRFiles.sh GJet_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU
 ./getTIFRFiles.sh TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_200PU
-./getFiles.sh QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU
+#./getFiles.sh QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU - check later, not available!
 ./getFiles.sh QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU
 ./getFiles.sh QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCUEP8M2T4_14TeV_Pythia8_200PU
-./getFiles.sh THQ_ctcvcp_HToGG_M125_TuneCUETP8M1_14TeV-madgraph-pythia8_200PU
+#./getFiles.sh THQ_ctcvcp_HToGG_M125_TuneCUETP8M1_14TeV-madgraph-pythia8_200PU - check later not available!
 ./getFiles.sh TTWJetsToLNu_TuneCUETP8M1_14TeV-amcatnloFXFX-madspin-pythia8_200PU
 ### ask for TTGjets###
 # WW inclusive not on DISK WW_TuneCUETP8M1_14TeV-pythia8_200PU
 ./getTIFRFiles.sh TTGG_0Jets_TuneCUETP8M1_14TeV_amcatnlo_madspin_pythia8_200PU
-./getFiles.sh DYJets_incl_MLL-50_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU
+#./getFiles.sh DYJets_incl_MLL-50_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU # check later not available! 
 ./getFiles.sh WGGJets_TuneCP5_14TeV_madgraphMLM_pythia8_200PU
 ./getFiles.sh WGJJToLNu_EWK_QCD_TuneCP5_14TeV-madgraph-pythia8_200PU
 ./getFiles.sh ZGTo2LG_TuneCUETP8M1_14TeV-amcatnloFXFX-pythia8_200PU
