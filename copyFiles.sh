450e6158ab6c6e04b2610a93d396ca09b73c0c45
Sandhya Jain (sandhya.jain@uclouvain.be) <sjain@ingrid-ui1.cism.ucl.ac.be>
2021-10-29 15:20:20 +0200

    adding scripts to copy samples locally

---
+++ copyFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+sample=$1
+echo $sample
+mkdir /nfs/user/sjain/$sample
+xrdfs root://cmseos.fnal.gov/ ls /store/user/snowmass/Snowmass2021/DelphesNtuplizer/$sample | awk -v s=$sample '{print "xrdcp root://cmseos.fnal.gov/"$1" /nfs/user/sjain/"$1}' > copy_$sample.txt
+chmod 755 copy_$sample.txt
+./copy_$sample.txt >& chk_$sample.txt &
