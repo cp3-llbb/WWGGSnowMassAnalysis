6983928b2c8dcb36779835e399449ece0a8e4bac
Suat Donertas (suat.donertas@uclouvain.be) <sdonerta@ingrid-ui1.cism.ucl.ac.be>
2021-11-05 14:32:55 +0100

    again more DNN variables

--- runCopyFiles.sh
+++ runCopyFiles.sh
@@ -1,14 +1,14 @@
-#./copyFiles.sh GluGluToHHTo2G2Qlnu_node_cHHH1_TuneCP5_14TeV-powheg-pythia8_200PU                                                                                        
+./copyFiles.sh GluGluToHHTo2G2Qlnu_node_cHHH1_TuneCP5_14TeV-powheg-pythia8_200PU                                                                                        
 #./copyFiles.sh W0JetsToLNu_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU                                                                                                 
 ./copyFiles.sh W1JetsToLNu_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU
 ./copyFiles.sh W2JetsToLNu_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU
 ./copyFiles.sh W3JetsToLNu_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU 
-####./copyTIFRFiles.sh DiPhotonJetsBox_MGG-80toInf_14TeV-Sherpa_200PU
+#./copyTIFRFiles.sh DiPhotonJetsBox_MGG-80toInf_14TeV-Sherpa_200PU
 ## Not on DISK ./copyFiles.sh DiPhotonJetsBox_MGG-40to80_14TeV-Sherpa_200PU 
 ./copyFiles.sh GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_14TeV-pythia8_200PU
 ./copyFiles.sh GJet_Pt-20toInf_DoubleEMEnriched_MGG-40to80_TuneCUEP8M2T4_14TeV_pythia8_200PU
-####./copyTIFRFiles.sh GJet_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU
-####./copyTIFRFiles.sh TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_200PU
+#./copyTIFRFiles.sh GJet_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU
+#./copyTIFRFiles.sh TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_200PU
 #./copyFiles.sh QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU - check later, not available!
 ./copyFiles.sh QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU
 ./copyFiles.sh QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCUEP8M2T4_14TeV_Pythia8_200PU
@@ -16,7 +16,7 @@
 ./copyFiles.sh TTWJetsToLNu_TuneCUETP8M1_14TeV-amcatnloFXFX-madspin-pythia8_200PU
 ### ask for TTGjets###
 # WW inclusive not on DISK WW_TuneCUETP8M1_14TeV-pythia8_200PU
-####./copyTIFRFiles.sh TTGG_0Jets_TuneCUETP8M1_14TeV_amcatnlo_madspin_pythia8_200PU
+#./copyTIFRFiles.sh TTGG_0Jets_TuneCUETP8M1_14TeV_amcatnlo_madspin_pythia8_200PU
 #./copyFiles.sh DYJets_incl_MLL-50_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU # check later not available! 
 ./copyFiles.sh WGGJets_TuneCP5_14TeV_madgraphMLM_pythia8_200PU
 ./copyFiles.sh WGJJToLNu_EWK_QCD_TuneCP5_14TeV-madgraph-pythia8_200PU
