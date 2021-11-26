ba59793c5877f3d40756c11973b067020e59a0c8
Suat Donertas (suat.donertas@uclouvain.be) <sdonerta@ingrid-ui1.cism.ucl.ac.be>
2021-11-26 10:13:13 +0100

    more tidying up

--- getTIFRFiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-sample=$1
-echo $sample
-xrdfs se01.indiacms.res.in ls /cms/store/group/Snowmass_2021_2022/DelphesNtuplizer/$sample | awk -v s=$sample '{print "root://se01.indiacms.res.in/"$1}' > $sample.txt
--- getTIFRLocalFiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-sample=$1
-echo $sample
-ls /nfs/user/sdonerta/cms/store/group/Snowmass_2021_2022/DelphesNtuplizer/$sample | awk -v s=$sample '{print "/nfs/user/sdonerta/cms/store/group/Snowmass_2021_2022/DelphesNtuplizer/'$sample'/"$1}' > $sample.txt
